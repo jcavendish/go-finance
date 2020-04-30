fde216fc299039e1f5419424d57f4b7920cc5cf9
Joao Paulo Cavendish <joaopaulocavendish@gmail.com>
2020-04-30 17:41:10 -0300

    fixing tests

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import filesize from 'filesize';
-
 import Header from '../../components/Header';
 import FileList from '../../components/FileList';
 import Upload from '../../components/Upload';
@@ -23,19 +21,27 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    data.append('file', uploadedFiles[0].file);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const filesToSend: FileProps[] = files.map(file => {
+      return {
+        file,
+        name: file.name,
+        readableSize: String(file.size),
+      };
+    });
+    setUploadedFiles([...filesToSend]);
   }
 
   return (
